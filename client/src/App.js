e90a0620cc535a7f7f18f342a657c86513748560
daniel <danielzinhors@gmail.com>
2020-07-14 20:07:10 -0300

    novo package react

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as api from './api/apiService.js';
+import Spinner from './components/Spinner.js';
 
 export default function App() {
-  return <h1>Desafio Final do Bootcamp Full Stack</h1>;
+  const [allTransactions, setallTransactions] = useState([]);
+  const [selectedTransaction, setSelectedTransaction] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const getTransaction = async () => {
+      const transaction = await api.getAllTransactions();
+      setTimeout(() => {
+        setallTransactions(transaction);
+      }, 2000);
+    };
+
+    // api.getAllGrades().then((grades) => {
+    //   setTimeout(() => {
+    //     setAllGrades(grades);
+    //   }, 2000);
+    // });
+
+    getTransaction();
+  }, []);
+
+  const handleDelete = async (gradeToDelete) => {
+    /*const isDeleted = await api.deleteGrade(gradeToDelete);
+
+    if (isDeleted) {
+      const deletedGradeIndex = allGrades.findIndex(
+        (grade) => grade.id === gradeToDelete.id
+      );
+
+      const newGrades = Object.assign([], allGrades);
+      newGrades[deletedGradeIndex].isDeleted = true;
+      newGrades[deletedGradeIndex].value = 0;
+
+      setAllGrades(newGrades);
+    }*/
+  };
+
+  const handlePersist = (grade) => {
+    //setSelectedGrade(grade);
+    // setIsModalOpen(true);
+  };
+
+  const handlePersistData = async (formData) => {
+    /* const { id, newValue } = formData;
+
+    const newGrades = Object.assign([], allGrades);
+
+    const gradeToPersist = newGrades.find((grade) => grade.id === id);
+    gradeToPersist.value = newValue;
+
+    if (gradeToPersist.isDeleted) {
+      gradeToPersist.isDeleted = false;
+      await api.insertGrade(gradeToPersist);
+    } else {
+      await api.updateGrade(gradeToPersist);
+    }
+
+    setIsModalOpen(false);*/
+  };
+
+  const handleClose = () => {
+    //    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <h1 className="center">Desafio Final do Bootcamp Full Stack</h1>
+      {allTransactions.length > 0 && <Spinner />}
+    </div>
+  );
 }
---
+++ client/src/api/apiService.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3001/api/transaction';
+
+async function getAllTransactions() {
+  const res = await axios.get(`${API_URL}/2019-02`);
+
+  const transactions = res.data.map((transaction) => {
+    const {
+      description,
+      value,
+      category,
+      year,
+      month,
+      day,
+      yearMonth,
+      yearMonthDay,
+      type,
+    } = transaction;
+
+    return {
+      ...transaction,
+      descriptionLowerCase: description.toLowerCase(),
+      categoryLowerCase: category.toLowerCase(),
+      isDeleted: false,
+    };
+  });
+
+  return transactions;
+}
+
+/*async function insertGrade(grade) {
+  const response = await axios.post(API_URL, grade);
+  return response.data.id;
+}
+
+async function updateGrade(grade) {
+  const response = await axios.put(API_URL, grade);
+  return response.data;
+}
+
+async function deleteGrade(grade) {
+  const response = await axios.delete(`${API_URL}/${grade.id}`);
+  return response.data;
+}
+
+async function getValidationFromGradeType(gradeType) {
+  const gradeValidation = GRADE_VALIDATION.find(
+    (item) => item.gradeType === gradeType
+  );
+
+  const { minValue, maxValue } = gradeValidation;
+
+  return {
+    minValue,
+    maxValue,
+  };
+}*/
+
+export { getAllTransactions };
+// insertGrade,
+// updateGrade,
+// deleteGrade,
+// getValidationFromGradeType,
