5ebc8e541ae3f3f150fdc49df7abad05e20a1627
daniel <danielzinhors@gmail.com>
2020-07-22 20:16:58 -0300

    ajustes na rotta da api no client

--- client/src/api/apiService.js
+++ client/src/api/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api/transaction';
+const API_URL = '/api/transaction';
 
 async function getAllTransactions(periodo) {
   const res = await axios.get(`${API_URL}${periodo}`);
